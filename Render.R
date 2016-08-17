3e28d5bf34f04aa4a1e69e8642f4303e16ff752a
Alex Singleton <alexandersingleton@gmail.com>
2016-08-17 14:54:13 +0100

    Website source update

--- Render.R
+++ Render.R
@@ -2,20 +2,11 @@ devtools::install_github('rstudio/rmarkdown')
 library(rmarkdown)
 setwd("~/Dropbox/Projects/Personal_Website")
 
-
-
-
-
-
-
 #Render the site
 render_site()
 
 #Creates a PDF of the CV
 render("cv.Rmd", "pdf_document",output_file="./_site/cv_Alex_Singleton.pdf")
-
-tufte_handout("cv.Rmd")
-
 
 #Inserts a download link to the PDF CV
 conn<-file("./_site/cv.html")
@@ -24,7 +15,6 @@ close(conn)
 d_link <- "<li><i class='fa fa-download' aria-hidden='true'></i><a href='cv_Alex_Singleton.pdf'> Download CV (pdf)</a></li>"
 text_new <- c(text[1:290],d_link,text[291:length(text)])
 write(text_new, file = "./_site/cv.html")
-
 
 #Shell Script to copy files and move update git
 system("bash git.sh")
