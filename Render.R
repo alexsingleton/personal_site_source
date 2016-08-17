e8714cbb3ab4fec727ef8e8d9b67a0ef1415f7ee
Alex Singleton <alexandersingleton@gmail.com>
2016-08-17 14:53:30 +0100

    Website source update

--- Render.R
+++ Render.R
@@ -26,30 +26,5 @@ text_new <- c(text[1:290],d_link,text[291:length(text)])
 write(text_new, file = "./_site/cv.html")
 
 
-#Copy Website & Source
-loc<- "/Users/alex/alexsingleton/"
-system(paste("cp -a",paste0(getwd(),"/_site/."), loc)) #Copy rendered website
-loc<- "/Users/alex/personal_site_source/"
-system(paste("cp -a",paste0(getwd(),"/."), loc)) #Copy source
-
-
-#GIT Website
-loc<- "/Users/alex/alexsingleton/"
-system(paste0("cd ",loc," && git add .")) #add files to the staging area
-system("git remote add origin ssh://git@github.com:alexsingleton/alexsingleton.github.io.git") #set the origin - note; need to setup the SSH
-system(paste0("cd ",loc," && git commit -m 'Website update'")) #commit changes
-system(paste0("cd ",loc," && git push -u origin master")) #Push
-
-#Source
-loc<- "/Users/alex/personal_site_source/"
-system(paste0("cd ",loc," && git add .")) #add files to the staging area
-system("git remote add origin ssh://git@github.com:alexsingleton/personal_site_source.git") #set the origin - note; need to setup the SSH
-system(paste0("cd ",loc," && git commit -m 'Website source update'")) #commit changes
-system(paste0("cd ",loc," && git push -u origin master")) #Push
-
-system("git remote set-url origin git@github.com:alexsingleton/personal_site_source.git")
-
-test2
-
-
-
+#Shell Script to copy files and move update git
+system("bash git.sh")
--- git.sh
+++ git.sh
@@ -1,5 +1,12 @@
+#Website Source
 cp -a /Users/alex/Dropbox/Projects/Personal_Website/. /Users/alex/personal_site_source/
 cd /Users/alex/personal_site_source/ && git add .
 git remote add origin ssh://git@github.com:alexsingleton/personal_site_source.git
 cd /Users/alex/personal_site_source/ && git commit -m 'Website source update'
 cd /Users/alex/personal_site_source/ && git push -u origin master
+#Website
+cp -a /Users/alex/Dropbox/Projects/Personal_Website/_site/. /Users/alex/alexsingleton/
+cd /Users/alex/alexsingleton/ && git add .
+git remote add origin ssh://git@github.com:alexsingleton/alexsingleton.github.io.git
+cd /Users/alex/alexsingleton/ && git commit -m 'Website update'
+cd /Users/alex/alexsingleton/ && git push -u origin master
